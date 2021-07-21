b77b03299e73e65318a27424b853120e49a9b796
Paulo <87743149+SKchima@users.noreply.github.com>
2021-07-21 16:28:26 -0300

    Add files via upload

---
+++ Fatorial.py
@@ -0,0 +1,15 @@
+print ("\n" "Farei o cálculo fatorial de um número de sua escolha!" "\n" "(Deve ser um número natural)")
+flagErro = True
+while flagErro:
+    valor_de_n = input("Qual o número desejado? ")
+    try:
+        float_n = float(valor_de_n)
+        n = int(valor_de_n)
+        p = 1
+        componentes = list(range(n, 0, -1))
+        for n in componentes:
+            p = p * n
+        print ("O resultado foi:", p)
+        flagErro = False
+    except:
+        print ("caracteres, vírgulas e espaços não são aceitos")
